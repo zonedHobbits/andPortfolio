c6a1fbc3ef332ecb5ab98113f92b9e15c46d30fd
freddetastic <fredde@dhcp-2-255.dhcp.hyperisland.local>
2013-10-09 10:28:26 +0200

    Added getters and setters to Person

--- src/com/example/zonedhobbitsportfolio/Person.java
+++ src/com/example/zonedhobbitsportfolio/Person.java
@@ -34,4 +34,68 @@ public class Person {
 		this.projects = projects;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getQuote() {
+		return quote;
+	}
+
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public Bitmap getNormal_img() {
+		return normal_img;
+	}
+
+	public void setNormal_img(Bitmap normal_img) {
+		this.normal_img = normal_img;
+	}
+
+	public Bitmap getFun_img() {
+		return fun_img;
+	}
+
+	public void setFun_img(Bitmap fun_img) {
+		this.fun_img = fun_img;
+	}
+
+	public String[] getContactInfo() {
+		return contactInfo;
+	}
+
+	public void setContactInfo(String[] contactInfo) {
+		this.contactInfo = contactInfo;
+	}
+
+	public Project[] getProjects() {
+		return projects;
+	}
+
+	public void setProjects(Project[] projects) {
+		this.projects = projects;
+	}
+
 }
