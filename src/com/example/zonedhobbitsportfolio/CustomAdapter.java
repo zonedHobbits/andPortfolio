84fea8537a07ab15cb3f41c67bf498d3d3941fd4
freddetastic <fredde@dhcp-2-255.dhcp.hyperisland.local>
2013-10-09 12:30:10 +0200

    Fixes in CustomAdapter

--- src/com/example/zonedhobbitsportfolio/CustomAdapter.java
+++ src/com/example/zonedhobbitsportfolio/CustomAdapter.java
@@ -1,7 +1,30 @@
 package com.example.zonedhobbitsportfolio;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+public class CustomAdapter extends ArrayAdapter<Object> {
+	
+	private Context context;
+	private Object[] values;
 
-public class CustomAdapter extends ArrayAdapter<Project> {
+	public CustomAdapter(Context context, int resource, Object[] values) {
+		super(context, resource, values);
+		// TODO Auto-generated constructor stub
+		this.context = context;
+		this.values = values;
+	}
+	
+	public View getView(int position, View convertView, ViewGroup parent){
+		
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.project_list_row, parent, false);
+		
+		return rowView;
+		
+	}
 
 }
