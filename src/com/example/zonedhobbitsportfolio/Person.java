a2ca5919ff813cab4124590a569a63a767d29841
freddetastic <fredde@dhcp-2-255.dhcp.hyperisland.local>
2013-10-09 10:26:14 +0200

    Add constructor to Person

--- src/com/example/zonedhobbitsportfolio/Person.java
+++ src/com/example/zonedhobbitsportfolio/Person.java
@@ -19,8 +19,19 @@ public class Person {
 	String[] contactInfo; //Email, phone, Twitter, URL
 	
 	Project[] projects;
-	
-	
 
+	public Person(String name, String quote, String nickName, String bio,
+			Bitmap normal_img, Bitmap fun_img, String[] contactInfo,
+			Project[] projects) {
+		super();
+		this.name = name;
+		this.quote = quote;
+		this.nickName = nickName;
+		this.bio = bio;
+		this.normal_img = normal_img;
+		this.fun_img = fun_img;
+		this.contactInfo = contactInfo;
+		this.projects = projects;
+	}
 
 }
