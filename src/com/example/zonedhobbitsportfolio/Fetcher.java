1578d431c3d5f9c6f7e21386ac49d57fb57250b7
freddetastic <fredde@dhcp-2-255.dhcp.hyperisland.local>
2013-10-09 12:46:56 +0200

    Main + Adapter

--- src/com/example/zonedhobbitsportfolio/Fetcher.java
+++ src/com/example/zonedhobbitsportfolio/Fetcher.java
@@ -1,3 +1,4 @@
+/*
 package com.example.zonedhobbitsportfolio;
 
 import android.os.AsyncTask;
@@ -11,3 +12,4 @@ public class Fetcher extends AsyncTask<Params, Progress, Result> {
 	}
 
 }
+*/
--- src/com/example/zonedhobbitsportfolio/MainActivity.java
+++ src/com/example/zonedhobbitsportfolio/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.zonedhobbitsportfolio;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	
+	ListView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        test = (ListView) findViewById(R.id.listView1);
+        Person[] arraypersons = new Person[1];
+        arraypersons[0] = new Person("Albin", null, null, null, null, null, null, null);
+        
+        CustomAdapter test1 = new CustomAdapter(this, test.getId(), arraypersons);
+        
+        test.setAdapter(test1);
+        
     }
 
 
