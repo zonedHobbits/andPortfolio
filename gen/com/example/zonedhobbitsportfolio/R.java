d6d8ec06bccd97f5afc1171b90ccb4b74657743e
freddetastic <fredde@dhcp-2-255.dhcp.hyperisland.local>
2013-10-09 16:14:14 +0200

    Added new class ProfileActivity.

--- gen/com/example/zonedhobbitsportfolio/R.java
+++ gen/com/example/zonedhobbitsportfolio/R.java
@@ -33,16 +33,19 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int main_list_row=0x7f030001;
-        public static final int project_list_row=0x7f030002;
+        public static final int activity_profile=0x7f030001;
+        public static final int main_list_row=0x7f030002;
+        public static final int project_list_row=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int profile=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_profile=0x7f050003;
     }
     public static final class style {
         /** 
