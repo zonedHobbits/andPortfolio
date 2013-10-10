d658649f6aaf07e1b0b7ae90150d08399687b287
Martin Haiek <martohaiek@gmail.com>
2013-10-10 17:41:09 +0200

    Revert 1dc7038..acf17ed
    This rolls back to commit 1dc70382c466132dac9b7e69a381070669d3aa25.

--- src/com/example/zonedhobbitsportfolio/MainActivity.java
+++ src/com/example/zonedhobbitsportfolio/MainActivity.java
@@ -3,21 +3,17 @@ package com.example.zonedhobbitsportfolio;
 import android.os.Bundle;
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Point;
-import android.graphics.Typeface;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.Menu;
-import android.view.View;
 import android.view.View.MeasureSpec;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
@@ -36,32 +32,25 @@ public class MainActivity extends Activity {
         
         // Just for test purposes
         
-        //arraypersons[0] = new Person("Albin", null, null, null, null, null, null, null, null, null, null, null);
-        //arraypersons[1] = new Person("Fredrik", null, null, null, null, null, null, null, null, null, null, null);
-        //arraypersons[2] = new Person("Martin", null, null, null, null, null, null, null, null, null, null, null);
+        arraypersons[0] = new Person("Albin", null, null, null, null, null, null, null, null, null, null, null);
+        arraypersons[1] = new Person("Fredrik", null, null, null, null, null, null, null, null, null, null, null);
+        arraypersons[2] = new Person("Martin", null, null, null, null, null, null, null, null, null, null, null);
         
         CustomAdapter test1 = new CustomAdapter(this, test.getId(), arraypersons);
         
         test.setAdapter(test1);
         
-        //Test fonts
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+        int width = displaymetrics.widthPixels;
         
-<<<<<<< HEAD
         Log.i("***HEIGHT", String.valueOf(height));
 
-        Person martin = null;
-        this.setUpInfo("http://puertosur.com.ar/Martin/andPorfolio/zhPortfolioAPI.php", martin);
-        //this.setUpInfo("http://fredrik-andersson.se/zh/zhPortfolioAPI.php");
-        //this.setUpInfo("http://alphahw.eu/zh/zhPortfolioAPI.php");
-=======
-        TextView myTextView=(TextView)findViewById(R.id.text_header_main);
-        Typeface typeFace=Typeface.createFromAsset(getAssets(),"fonts/Edmondsans-Bold.otf");
-        myTextView.setTypeface(typeFace);
 
         this.setUpInfo("http://puertosur.com.ar/Martin/andPorfolio/zhPortfolioAPI.php");
         this.setUpInfo("http://fredrik-andersson.se/zh/zhPortfolioAPI.php");
         this.setUpInfo("http://alphahw.eu/zh/zhPortfolioAPI.php");
->>>>>>> 1dc70382c466132dac9b7e69a381070669d3aa25
 
     }
 
@@ -73,17 +62,8 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void setUpInfo(String url, Person name) {
-    	new Fetcher(name).execute(url);
-    }
-    
-    public void moveToProfile(View v){
-    	
-    	Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
-        
-        //We need to pass through some kind of data to know which profile clicked.
-    
+    public void setUpInfo(String url) {
+    	new Fetcher().execute(url);
     }
     
 }
