d04fc2eb567a706564ded19f266d9bbf23a8fdfd
alphahw <albin_wilen@msn.com>
2013-10-11 16:03:50 +0200

    Beginning to throw values into the interface using CustomAdapter

--- src/com/example/zonedhobbitsportfolio/CustomAdapter.java
+++ src/com/example/zonedhobbitsportfolio/CustomAdapter.java
@@ -6,11 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CustomAdapter extends ArrayAdapter<Object> {
 	
 	private Context context;
 	private Object[] values;
+	private int Iterator;
 	
 	private int checkId;
 	private int setLayout;
@@ -31,7 +34,14 @@ public class CustomAdapter extends ArrayAdapter<Object> {
 		View rowView = inflater.inflate(setLayout(), parent, false);
 		
 		// Fill fields
-		fillLayout();
+		fillLayout(rowView);
+		
+		if(Integer.toString(Iterator) == null) {
+			Iterator = 0;
+		}
+		if(Iterator <= (values.length - 2)) {
+			Iterator++;
+		}
 		
 		return rowView;
 		
@@ -54,11 +64,19 @@ public class CustomAdapter extends ArrayAdapter<Object> {
 		return 0;
 	}
 	
-	public void fillLayout(){
+	public void fillLayout(View rowView){
 		
 		if(checkId == R.id.list_main) {
 			//FILL FIELDS
 			
+			ImageView normalImg = (ImageView) rowView.findViewById(R.id.imageView);
+			TextView name = (TextView) rowView.findViewById(R.id.name);
+			TextView nickName = (TextView) rowView.findViewById(R.id.nickName);
+			
+			Log.i("******Values["+String.valueOf(Iterator)+"]", ((Person) values[Iterator]).getName());
+						
+			//normalImg.setImageBitmap(values[0]);
+
 		}
 		/*
 		else if(checkId == R.id.list_profile) {
