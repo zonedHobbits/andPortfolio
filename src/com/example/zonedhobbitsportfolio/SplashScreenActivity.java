21a2cffdb694617931997c2fda3be4f6df8898b0
Martin Haiek <martohaiek@gmail.com>
2013-10-21 10:44:22 +0200

    center ring

--- src/com/example/zonedhobbitsportfolio/SplashScreenActivity.java
+++ src/com/example/zonedhobbitsportfolio/SplashScreenActivity.java
@@ -1,7 +1,12 @@
 package com.example.zonedhobbitsportfolio;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.app.Activity;
+import android.graphics.Color;
 
 public class SplashScreenActivity extends Activity {
 
@@ -11,9 +16,24 @@ public class SplashScreenActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    RelativeLayout superView = new RelativeLayout(this);
     
-    GifWebView view = new GifWebView(this, "file:///android_asset/ring.gif"); 
-    setContentView(view);
+    setContentView(superView);
+    
+    GifWebView view = new GifWebView(this, "file:///android_asset/ring.gif");
+    
+    superView.addView(view);
+    
+    superView.setBackgroundColor(Color.BLACK);
+    view.setBackgroundColor(Color.BLACK);
+    
+    view.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+    
+    RelativeLayout.LayoutParams lp = (LayoutParams) view.getLayoutParams();
+    lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+    
+    view.setLayoutParams(lp);
     
     activity = this;
   }
@@ -22,4 +42,7 @@ public class SplashScreenActivity extends Activity {
 	  activity.finish();
   }
   
+  public void padding(View view) {
+	  view.setPadding(20,20,20,20);
+  }
 }
